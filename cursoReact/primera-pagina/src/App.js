67ad3e1151b83ac4fc5445f69a7f1190ecfcf43d
Sebastian Carrero <mugssebastian@gmail.com>
2023-08-10 23:41:53 -0500

    1.1

--- cursoReact/primera-pagina/src/App.js
+++ cursoReact/primera-pagina/src/App.js
@@ -3,11 +3,25 @@ import Menu from './components/menu';
 import './App.css';
 
 function App() {
-  const Saludo = "Hola como estas"
+  const currentUrl = window.location.pathname
+  function renderContent(){
+    switch(currentUrl){
+      case "/":
+        break;
+      case "/personajes":
+        break;
+      case "/episodios":
+        break;
+      case "/citas":
+        break;
+      default:
+        break;
+    }
+  }
   return (
     <div className="App">
       <Menu/>
-      
+      {renderContent()}
     </div>
   );
 }
