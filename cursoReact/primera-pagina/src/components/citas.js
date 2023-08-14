6fd35889fbdeb5968091163574d9989ec3b1de9b
Sebastian Carrero <mugssebastian@gmail.com>
2023-08-13 23:31:06 -0500

    fin curso

--- cursoReact/primera-pagina/src/components/citas.js
+++ cursoReact/primera-pagina/src/components/citas.js
@@ -1,11 +1,19 @@
 import {useEffect, useState} from 'react';
 
 function Citas(){
-    const [quote, setQuote] = useState('Hola soy una cita')
-    useEffect(function(){
-
+    const [quote, setQuote] = useState(null)
+    useEffect(function getQuotes(){
+        fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+        .then(response => response.json())  // convertir a json
+        .then(data => setQuote(data.data))
+         //imprimir los datos en la consola
+        
     },[])
-    return(<div>{quote}</div>)
+    return(
+        <div>
+            <p>{quote?.name}</p>
+        </div>
+    )
 }
 
 export default Citas
